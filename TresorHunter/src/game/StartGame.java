0db015fb964e2293d7ece6f6a57da0375ab73c1e
ToukaM <t_merouani@outlook.fr>
2019-05-10 15:52:04 +0200

    Game totaly implemented

--- TresorHunter/src/game/StartGame.java
+++ TresorHunter/src/game/StartGame.java
@@ -8,18 +8,42 @@ public class StartGame {
 		
 	}
 	
-	public boolean gameProcess() {
-		boolean result = false;
+	public void gameProcess() {
 		grid = new Grid();
 		movement = new Movement(grid);
+		instructions();
+		movementInstructions();
+		information();
 		grid.displayGrid();
-		while(((grid.getPersona().getLife().getValue())>0)&&((grid.getPersona().getInventory().getCoinsCollected())<=5)) {
+		while(((grid.getPersona().getLife().getValue())>0)&&((grid.getPersona().getInventory().getCoinsCollected())<5)) {
 			movement.move();
+			movementInstructions();
+			information();
 			grid.displayGrid();
-			System.out.println(grid.getPersona().getLife().getValue());
-			System.out.println(grid.getPersona().getInventory().getCoinsCollected());
 		}
-		return result;
+		if((grid.getPersona().getLife().getValue())==0){
+			System.out.println("You lost");
+		}
+		else if((grid.getPersona().getInventory().getCoinsCollected())==5) {
+			System.out.println("Congratulations !! you won");
+		}
+	}
+	
+	public void instructions() {
+		System.out.println("Welcome to treasure hunter");
+		System.out.println("To win you will have to collect 5 treasures without dying");
+		System.out.println("1 is your character, 2 are traps, 3 are treasures");
+	}
+	
+	public void movementInstructions() {
+		System.out.println("                   To move up use : z");
+		System.out.println("To move left use : q                    to move right use : d");
+		System.out.println("                   To move down use : s");
+	}
+	
+	public void information() {
+		System.out.print("Life = "+grid.getPersona().getLife().getValue()+"");
+		System.out.println("  |  Treasures collected = "+grid.getPersona().getInventory().getCoinsCollected()+"");
 	}
 	
 }
